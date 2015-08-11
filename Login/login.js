c2ef1d8235895ef347c5af2ac588073d22b5e66e
Humdaan Mustafa <humishum@gmail.com>
2015-08-10 19:09:33 -0700

    Added Angular routing

---
+++ Login/login.js
@@ -0,0 +1,28 @@
+var login = angular.module('login', ['ngRoute']);
+
+
+login.config(function($routeProvider) {
+  
+        $routeProvider
+        // initital route for login page
+        .when('/', {
+            templateUrl :'Login/login.html',
+            controller  : 'loginController'
+        })
+        
+        //route for signup page
+        .when('/signup',    {
+            templateUrl : 'Login/signup.html',
+            controller  : 'signupController'
+        });
+})  ;
+
+login.controller('loginController', function($scope) {
+    
+    $scope.message = 'HEllo World' ;
+});
+
+login.controller('signupcontroller', function($scope) {
+    
+    $scope.message = ' Signup now ya fools' ;
+});
